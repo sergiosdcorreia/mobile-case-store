ab7e373c861fade522b9098af4fd5019bba8d3cb
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-07-02 00:13:29 +0100

    Add images, logo and heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,16 @@ export default function Home() {
     <div className="bg-slate-50">
       <section>
         <MaxWithWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
-          <div className="col-span-2 px-6 lg:px-0 lg:pt-4"></div>
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 top-20 hidden lg:block">
+                <img src="/snake-1.png" className="w-full" />
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a <span>Custom</span> Phone Case
+              </h1>
+            </div>
+          </div>
         </MaxWithWrapper>
       </section>
     </div>
